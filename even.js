7592b39abfa80b47214c24ce3a30f754221ca929
arlet2020 <arlet_vazquez@hotmail.com>
2020-12-23 16:55:21 -0500

    learning functions

---
+++ even.js
@@ -0,0 +1,34 @@
+/*
+
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+ const bueno = isEven(21);
+ console.log(bueno);
+
+ */
+
+// MAS EJERCICIOS CON RETURN STATEMENT Y EVALUANDO EXPRESSIONES PARA FALSE O VERDADERO
+
+/*
+
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
+*/
+
+// TMB PUEDO SIMPLICAR EL CODIGO...
+
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
--- hello.js
+++ hello.js
@@ -14,3 +14,31 @@ sayHolle("Linda");
 sayHolle("Arturo");
 sayHolle("Walter");
 
+// ESTE EJERCICIO MUESTRA LA DIF % CONSOLE Y RETURN
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+
+
+// EL DE ABAJO USA RETURN EN VEZ DE CONSOLE 
+// NO IMPRIME NADA EN LA TERMINAL
+// PERO LO PUEDO INVOCAR DESPUES CON CONSOLE
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('Bicho');
+console.log(greeting);
+
+// 
+
+
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
