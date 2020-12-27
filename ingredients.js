8ab801d49259ca783b281e8922136c88ef11c793
arlet2020 <arlet_vazquez@hotmail.com>
2020-12-27 07:15:40 -0500

    my exercise

---
+++ ingredients.js
@@ -0,0 +1,22 @@
+const ingredients = ['eggs', 'milk', 'sugar', 'baking soda', 'baking powder', 'chocolate chips', 'bananas'];
+
+console.log('Banana bread ingredients:');
+for (i = 0; i < ingredients.length; i++) {
+   console.log(ingredients[i]);
+}
+
+
+console.log('Banana bread ingredients");
+let i = 0;
+while (i < ingredients.length) {
+   console.log(ingredients[i]);
+   i++
+}
+
+
+cons reverse = ingredients.reverse()
+
+console.log('Banana bread ingredients:");
+for (i = 0; i < reverse.length; i++) {
+   console.log(reverse[i];
+}
