14f610d702c1a18aff291afef1566ba9c85a5553
arlet2020 <arlet_vazquez@hotmail.com>
2020-12-27 12:28:33 -0500

    my module4 exercises

--- vegetal.js
+++ vegetal.js
@@ -1,13 +1,33 @@
 let judgeVegetable = function (vegetables, metric) {
-  let name = '';
-  let number = 0;
-  for (let i = 0; i < vegetables.length; i++) {
-    if (vegetables[i][metric] > number) {
-      number = vegetables[i][metric];
-      name = vegetables[i].submitter;
+  let name = ''; 
+  let number = 0; 
+  for (let i = 0; i < vegetables.length; i++) { 
+    if (vegetables[i][metric] > number) { 
+      number = vegetables[i][metric]; 
+      name = vegetables[i].submitter; 
     }
   }
-  return name
+  return name 
 };
 
-console.log(judgeVegetable('Old man Franklin'))
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3
+  }
+]
+
+const metric = 'redness'; 
+
+console.log(judgeVegetable(vegetables, metric));
