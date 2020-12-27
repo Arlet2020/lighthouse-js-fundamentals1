03fee8e4082f7f4138c7deb47e6331f1aa8fb3ce
arlet2020 <arlet_vazquez@hotmail.com>
2020-12-27 13:10:07 -0500

    tinkering

---
+++ exercises.js
@@ -0,0 +1,11 @@
+/* The function returns one of the following strings, based on the value of age:
+
+Elementary School if age is below 13
+Secondary School if age is between 13 and 18 (both inclusive)
+Lighthouse Labs in all other cases.
+*/
+
+//const whichSchool = function (age) //{
+ // if (age <= 13)
+  
+//};
---
+++ tinkering.js
@@ -0,0 +1,95 @@
+function multiply(a,b) {
+    return a * b;
+}
+
+const result = multiply(2,4);
+
+console.log(result);
+
+
+console.log(3);
+
+console.log(25);
+
+console.log(3 + 2.1);
+
+console.log(2 + 10 - 19 + 4 - 90 + 1);
+
+console.log(-20 + -19 -(-10)-(-1) + 24);
+
+console.log((10/5) * 4 - 20);
+
+console.log(4096%12);
+
+console.log(5 > 10);
+
+console.log(5 < 10);
+
+console.log(5 == 10);
+
+console.log(43 > 47);
+
+console.log(4 + 4);
+
+console.log(12 == 17);
+
+console.log(3 <=3);
+
+console.log(1 != 0);
+
+console.log(10 * 4 + 4 - 2);
+
+console.log(`hello world`);
+
+console.log('hello' + 'linda');
+
+console.log('prueba');
+
+console.log('Hello + 5*10');
+
+console.log('Hello' + 5*10);
+
+var name = 'James';
+console.log(name[1]);
+
+var jol = 'Bebe Russ';
+console.log(jol[5]);
+
+console.log('The bebe said : \'hola amigo.\'');
+
+console.log('Up up \n\t down down');
+
+console.log('sorry');
+
+
+console.log('Hello' % 10);
+
+
+console.log( "3" > 1);
+//console.log( 3! = "3");
+console.log(true > 0);
+console.log(1 !== false);
+console.log("false" === 0);
+console.log( 3 === 3);
+
+
+const raining = true;
+
+if (raining) {
+    console.log("Don't forget your umbrella");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
